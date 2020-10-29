ebad958fc4e1ecee41a061bf1d586c07adfd2d3d
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2020-10-29 16:21:04 +0000

    Fixed movement

--- src/Main.gd
+++ src/Main.gd
@@ -1,11 +1,6 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,12 +8,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("ui_left"):
-		$Player.velocity.x = -200
-	elif Input.is_action_pressed("ui_right"):
-		$Player.velocity.x = 200
-	if Input.is_action_pressed("jump"):
-		$Player.velocity.y = -200
+	#if Input.is_action_pressed("ui_left"):
+	#	$Player.velocity.x = -200
+	#elif Input.is_action_pressed("ui_right"):
+	#	$Player.velocity.x = 200
+	#if Input.is_action_pressed("jump"):
+#		$Player.velocity.y = -200
 		
 	pass
 
--- src/Player.gd
+++ src/Player.gd
@@ -9,6 +9,18 @@ func _ready():
 	pass # Replace with function body.
 
 
+func _process(delta):
+	if Input.is_action_pressed("ui_left"):
+		$AnimatedSprite.animation = "left"
+		$AnimatedSprite.play()
+		velocity.x = -200
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = 200
+		$AnimatedSprite.animation = "right"
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+				
 func _input(event):
 	if event.is_action_pressed("jump") && can_jump:
 		velocity.y = -GRAVITY * 50
@@ -17,14 +29,9 @@ func _input(event):
 func _physics_process(delta):
 	velocity.y += GRAVITY
 	
-	if velocity.x < 0:
-		$AnimatedSprite.animation = "left"
-		$AnimatedSprite.play()
-	elif velocity.x > 0:
-		$AnimatedSprite.animation = "right"
-		$AnimatedSprite.play()
-	else:
-		$AnimatedSprite.stop()
+	#if velocity.x < 0:
+	#elif velocity.x > 0:
+	#else:
 		
 	# Move up and down first
 	var motion = velocity * delta
