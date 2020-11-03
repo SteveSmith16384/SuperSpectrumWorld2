f83f1b9ab95f45af97abef1ec983020b891d0b94
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2020-11-03 15:16:16 +0000

    Added collectables

--- src/systems/MovementSystem.gd
+++ src/systems/MovementSystem.gd
@@ -60,10 +60,16 @@ func process_collision(player, other, collision):
 	if ECS.entity_has_component(other.id, "harmsplayercomponent"):
 		#var c = ECS.entity_get_component(other.id, "harmsplayercomponent")
 		if (collision.normal.y < 0):
-			ECS.remove_entity(other)
+			ECS.remove_entity(other) # todo
 		else:
 			print("Player killed")
 	
+	if ECS.entity_has_component(other.id, "removeoncollisioncomponent"):
+		ECS.remove_entity(other)
+	
+	if ECS.entity_has_component(other.id, "incscoreoncollision"):
+		# todo
+		pass
 	
 	
 	
