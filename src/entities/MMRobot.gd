c21517f4fccbea64444f78e9b5bfce9f80d2067d
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2020-11-07 16:31:32 +0000

    Fixed robot anim position

--- src/entities/MMRobot.gd
+++ src/entities/MMRobot.gd
@@ -1,6 +1,7 @@
 extends Entity
 
 var seen = false
+#var frame_id = 0;
 
 func _ready():
 	var m = get_node("Components/MovementComponent")
@@ -9,3 +10,11 @@ func _ready():
 	var ai = get_node("Components/HasAIComponent")
 	ai.move_left = true
 
+
+
+func _on_AnimatedSprite_frame_changed():
+	#frame_id = frame_id + 1
+	
+	var new_offset = $CollisionShape2D.position.x + ($AnimatedSprite.frame * -2)
+	$AnimatedSprite.offset.x = new_offset
+	pass # Replace with function body.
