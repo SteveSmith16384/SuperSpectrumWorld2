49b7a226f4359745517f4dd5197b4235f3617f84
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2020-11-29 09:53:15 +0000

    Finished HA tilemap

---
+++ src/levels/Level1_Army.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	VisualServer.set_default_clear_color( Color( 0, .97, .97, 1 ))
+	pass # Replace with function body.
+
