ae14375b1da98809aafb750c6e3173af2bf0fad2
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2020-12-02 11:21:50 +0000

    Added HA plane

--- src/Globals.gd
+++ src/Globals.gd
@@ -3,7 +3,9 @@ extends Node
 const ENEMY_TANK = 0
 const ENEMY_EXOLON_ROCKET = 1
 const ENEMY_AIRWOLF = 2
+const ENEMY_HARRIER_ATTACK_PLANE = 3
 
 static func throw_ex(msg : String):
 	push_error(msg)
-	var x = 10 / 10
+	var y = 0
+	var x = 10 / y
--- src/entities/EnemySpawner.gd
+++ src/entities/EnemySpawner.gd
@@ -7,6 +7,11 @@ export var count : int = 1
 
 var onscreen = false
 
+func _ready():
+	get_node("Sprite").visible = false
+	pass
+	
+
 func _on_VisibilityNotifier2D_screen_entered():
 	onscreen = true
 	pass # Replace 
@@ -24,20 +29,31 @@ func _on_Timer_timeout():
 				return
 			count = count - 1
 
+		var scene = null
+		
 		match type:
 			Globals.ENEMY_TANK:
 				pass
 			Globals.ENEMY_EXOLON_ROCKET:
-				var scene = load("res://entities/ExolonRocketEntity.tscn")
-				var bullet = scene.instance()
-				bullet.position.x = bullet.position.x + 100
-				add_child(bullet)
+				scene = load("res://entities/ExolonRocketEntity.tscn")
+				#var bullet = scene.instance()
+				#bullet.position.x = bullet.position.x + 100
+				#add_child(bullet)
+				
 			Globals.ENEMY_AIRWOLF:
-				var scene = load("res://entities/AirwolfEnemy.tscn")
-				var bullet = scene.instance()
-				bullet.position.x = bullet.position.x + 200
-				add_child(bullet)
+				scene = load("res://entities/AirwolfEnemy.tscn")
+				#var bullet = scene.instance()
+				#bullet.position.x = bullet.position.x + 200
+				#add_child(bullet)
+				
+			Globals.ENEMY_HARRIER_ATTACK_PLANE:
+				scene = load("res://entities/HarrierAttackPlane.tscn")
+				
 			_:
 				Globals.throw_ex("Unhandled enemy: " + str(type))
+
+		var bullet = scene.instance()
+		add_child(bullet)
+		bullet.position.x = bullet.position.x + 100
 		
 	pass
